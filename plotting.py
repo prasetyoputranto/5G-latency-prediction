7dd4040f675c5edd7fa284a9f3838bbb34770b37
pras <44287903+prasetyoputranto@users.noreply.github.com>
2022-11-18 12:31:04 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -9,7 +9,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ""
 
 from pr3d.de import ConditionalGaussianMM
 from pr3d.de import ConditionalGammaMixtureEVM
-condition_labels = ['queue_length_h0'] #,'longer_delay_prob_h1','queue_length_h0','queue_length_h1']
+condition_labels = ['queue_length_h0','queue_length_h1']
 y_label = 'end2end_delay'
 
 df_train = pd.read_parquet('dataset.parquet')
@@ -19,9 +19,14 @@ df_train = df_train[
         *condition_labels
     ]
 ]
-
+df_train = df_train[
+    df_train.queue_length_h0 >= 0
+]
+df_train = df_train[
+    df_train.queue_length_h1 >= 0
+]
 df_train2=np.array(df_train)
-train = df_train2[(df_train2[:,1]==3)]
+train = df_train2[(df_train2[:,1]==1) & (df_train2[:,2]==5)]
 df_train = df_train[[y_label, *condition_labels]]
 df_train["y_input"] = df_train[y_label]
 df_train = df_train.drop(columns=[y_label])
@@ -29,8 +34,8 @@ df_train = df_train.drop(columns=[y_label])
 # Gaussian modal
 model = ConditionalGaussianMM(
     x_dim=condition_labels,
-    centers=2,
-    hidden_sizes=[8,8],
+    centers=3,
+    hidden_sizes=[8,8,8],
     dtype="float32",
 )
 batch_size = 1000
@@ -39,35 +44,28 @@ Y = df_train.y_input
 steps_per_epoch = len(df_train) // batch_size
 model.training_model.compile(
     optimizer=tf.keras.optimizers.Adam(
-        learning_rate=0.0005,
+        learning_rate=0.0002,
     ),
     loss=model.loss,
 )
+X = np.array(X)
+Y = np.array(Y)
+training_data = tuple([ X[:,i] for i in range(len(condition_labels)) ]) + (Y,)
 model.training_model.fit(
-    x=[X, Y],
+    x=training_data,
     y=Y,
     steps_per_epoch=steps_per_epoch,
-    epochs=500,
+    epochs=2500,
     verbose=1,
 )
-x1=np.array([[3],[3]])
-y=np.arange(200)
+x1=np.array([[1,5],[1,5]])
+y=np.arange(250)
 p1=np.zeros(y.size)
 for i in range(0,y.size-1,2):
     yr=np.array([y[i],y[i+1]])
     prob, logprob, pred_cdf = model.prob_batch(x1, yr)
     p1[i]=prob[0]
     p1[i+1]=prob[1]
-#Try another condition
-# x2=np.array([[10],[10]])
-# p2=np.zeros(y.size)
-# for i in range(0,y.size-1,2):
-#     yr2=np.array([y[i],y[i+1]])
-#     prob, logprob, pred_cdf = model.prob_batch(x2, yr2)
-#     p2[i]=prob[0]
-#     p2[i+1]=prob[1]
-
-
 
 # Gamma modal
 model = ConditionalGammaMixtureEVM(
@@ -76,25 +74,22 @@ model = ConditionalGammaMixtureEVM(
     hidden_sizes=[8,8,8],
     dtype="float32",
 )
-batch_size = 1000
-X_Gamma = df_train[condition_labels]
-Y_Gamma = df_train.y_input
-steps_per_epoch = len(df_train) // batch_size
+
 model.training_model.compile(
     optimizer=tf.keras.optimizers.Adam(
-        learning_rate=0.0005,
+        learning_rate=0.002,
     ),
     loss=model.loss,
 )
 model.training_model.fit(
-    x=[X, Y],
+    x=training_data,
     y=Y,
     steps_per_epoch=steps_per_epoch,
-    epochs=300,
+    epochs=1500,
     verbose=1,
 )
-x_Gamma=np.array([[3],[3]])
-y_Gamma=np.arange(200)
+x_Gamma=np.array([[1,5],[1,5]])
+y_Gamma=np.arange(250)
 p_Gamma=np.zeros(y_Gamma.size)
 for i in range(0,y_Gamma.size-1,2):
     yr_Gamma=np.array([y_Gamma[i],y_Gamma[i+1]])
@@ -102,11 +97,11 @@ for i in range(0,y_Gamma.size-1,2):
     p_Gamma[i]=prob[0]
     p_Gamma[i+1]=prob[1]
 
-
+print(train)
 # Plot
 plt.plot(y_Gamma, p_Gamma)
 plt.plot(y, p1)
 plt.xlabel('delay')
 plt.ylabel('probability')
 plt.legend(['Gamma', 'Gaussian'], loc='upper right')
-plt.hist(train[:,0], color=['lightgreen'], density=True, bins=30)
+plt.hist(train[:,0], color=['lightgreen'], density=True, bins=60)
